18e90923ee4649054bb8e0a58ab2054795faee37
Minroe <scott.whitney334@gmail.com>
2020-07-22 20:49:57 -0700

    React Router

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
+import { Route } from 'react-router-dom';
 import Homepage from './Component/HomePage-component/homepage-component';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <Homepage/>
+      <Route exact path ='/' component={HomePage}/>
+      
     </div>
   );
 }
